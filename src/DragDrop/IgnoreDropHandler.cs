170a8e4e51040f606efd74068601ec4adf967e15
Mads Kristensen <post@madskristensen.net>
2016-05-12 15:55:01 -0700

    Added drag 'n drop

---
+++ src/DragDrop/IgnoreDropHandler.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Editor.DragDrop;
+
+namespace IgnoreFiles
+{
+    internal class IgnoreDropHandler : IDropHandler
+    {
+        private IWpfTextView _view;
+        private string _draggedFileName;
+        private string _documentFileName;
+
+        public IgnoreDropHandler(IWpfTextView view, string fileName)
+        {
+            _view = view;
+            _documentFileName = fileName;
+        }
+
+        public DragDropPointerEffects HandleDataDropped(DragDropInfo dragDropInfo)
+        {
+            var position = dragDropInfo.VirtualBufferPosition.Position;
+            var line = _view.GetTextViewLineContainingBufferPosition(position);
+            string text = PackageUtilities.MakeRelative(_documentFileName, _draggedFileName)
+                                          .Replace("\\", "/");
+
+            // Insert a new line if dragged after existing text
+            if (line.Start < position)
+                text = Environment.NewLine + text;
+
+                using (var edit = _view.TextBuffer.CreateEdit())
+            {
+                edit.Insert(position, text);
+                edit.Apply();
+            }
+
+            return DragDropPointerEffects.Copy;
+        }
+
+        public void HandleDragCanceled()
+        { }
+
+        public DragDropPointerEffects HandleDragStarted(DragDropInfo dragDropInfo)
+        {
+            return DragDropPointerEffects.All;
+        }
+
+        public DragDropPointerEffects HandleDraggingOver(DragDropInfo dragDropInfo)
+        {
+            return DragDropPointerEffects.All;
+        }
+
+        public bool IsDropEnabled(DragDropInfo dragDropInfo)
+        {
+            _draggedFileName = GetImageFilename(dragDropInfo);
+
+            return File.Exists(_draggedFileName) || Directory.Exists(_draggedFileName);
+        }
+
+        private static string GetImageFilename(DragDropInfo info)
+        {
+            var data = new DataObject(info.Data);
+
+            if (info.Data.GetDataPresent("FileDrop"))
+            {
+                // The drag and drop operation came from the file system
+                var files = data.GetFileDropList();
+
+                if (files != null && files.Count == 1)
+                {
+                    return files[0];
+                }
+            }
+            else if (info.Data.GetDataPresent("CF_VSSTGPROJECTITEMS"))
+            {
+                // The drag and drop operation came from the VS solution explorer
+                return data.GetText();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/DragDrop/IgnoreDropHandlerProvider.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Editor.DragDrop;
+using Microsoft.VisualStudio.Utilities;
+
+namespace IgnoreFiles
+{
+    [Export(typeof(IDropHandlerProvider))]
+    [DropFormat("CF_VSSTGPROJECTITEMS")]
+    [DropFormat("text")]
+    [DropFormat("FileDrop")]
+    [Name("IgnoreDropHandler")]
+    [ContentType(IgnoreContentTypeDefinition.IgnoreContentType)]
+    [Order(Before = "DefaultFileDropHandler")]
+    internal class IgnoreDropHandlerProvider : IDropHandlerProvider
+    {
+        [Import]
+        ITextDocumentFactoryService TextDocumentFactoryService { get; set; }
+
+        public IDropHandler GetAssociatedDropHandler(IWpfTextView view)
+        {
+            ITextDocument document;
+
+            if (TextDocumentFactoryService.TryGetTextDocument(view.TextBuffer, out document))
+            {
+                return view.Properties.GetOrCreateSingletonProperty(() => new IgnoreDropHandler(view, document.FilePath));
+            }
+
+            return null;
+        }
+    }
+}
