7e1f10d61bea252791420b11b06c80638ebbaa31
Mads Kristensen <post@madskristensen.net>
2016-05-13 12:40:43 -0700

    Updated description and tags

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -9,10 +9,10 @@ namespace IgnoreFiles
     {
         public const string Id = "7ac24965-ea21-4108-9cac-6e46394aaaef";
         public const string Name = ".ignore";
-        public const string Description = "A lanuage service for dealing with all types of .ignore files such as .gitignore, .tfignore etc.";
+        public const string Description = "A lanuage service that makes it painless to handle all types of .ignore files such as .gitignore, .tfignore etc. ";
         public const string Language = "en-US";
         public const string Version = "1.0";
         public const string Author = "Mads Kristensen";
-        public const string Tags = "git, tfs, svn, mercurial, ignore, npm,";
+        public const string Tags = "git, tfs, svn, mercurial, ignore, npm, scc";
     }
 }
