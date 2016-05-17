93f3d9e37e569accd8e06552649e88e799d8c4d2
Mads Kristensen <post@madskristensen.net>
2016-05-17 15:22:38 -0700

    Refactored the logger

--- src/Helpers/Logger.cs
+++ src/Helpers/Logger.cs
@@ -1,52 +1,42 @@
 using System;
-using System.Diagnostics;
 using Microsoft.VisualStudio.Shell.Interop;
 
-public class Logger : TraceListener
+public static class Logger
 {
-    private string _name;
-    private IVsOutputWindowPane pane;
-    private static object _syncRoot = new object();
-    private IServiceProvider _provider;
-
-    private Logger(IServiceProvider provider, string name)
-    {
-        _provider = provider;
-        _name = name;
-    }
+    private static string _name;
+    private static IVsOutputWindowPane _pane;
+    private static IVsOutputWindow _output;
 
     public static void Initialize(IServiceProvider provider, string name)
     {
-        var logger = new Logger(provider, name);
-
-        Trace.Listeners.Add(logger);
-        Trace.AutoFlush = true;
+        _output = (IVsOutputWindow)provider.GetService(typeof(SVsOutputWindow));
+        _name = name;
     }
 
-    public static void Log(object message) =>
-        Trace.Write(message);
-
-    public override void WriteLine(string message) =>
-        Write(message);
-
-    public override void Write(string message)
+    public static void Log(object message)
     {
-        if (EnsurePane())
+        try
         {
-            pane.OutputString(DateTime.Now.ToString() + ": " + message + Environment.NewLine);
+            if (EnsurePane())
+            {
+                _pane.OutputString(DateTime.Now.ToString() + ": " + message + Environment.NewLine);
+            }
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.Write(ex);
         }
     }
 
-    private bool EnsurePane()
+    private static bool EnsurePane()
     {
-        if (pane == null)
+        if (_pane == null)
         {
             Guid guid = Guid.NewGuid();
-            IVsOutputWindow output = (IVsOutputWindow)_provider.GetService(typeof(SVsOutputWindow));
-            output.CreatePane(ref guid, _name, 1, 1);
-            output.GetPane(ref guid, out pane);
+            _output.CreatePane(ref guid, _name, 1, 1);
+            _output.GetPane(ref guid, out _pane);
         }
 
-        return pane != null;
+        return _pane != null;
     }
 }
